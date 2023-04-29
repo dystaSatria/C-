74d7c75a1d61b266ec25309be70dc863c0dd11df
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-04-29 22:35:33 +0300

    Create faktorial.c

---
+++ faktorial.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Faktorial (Call by Reference)
+                            
+*******************************************************************************/
+
+#include <stdio.h>
+int factorial(int);
+
+
+int main()
+{
+    int x;
+    printf("Input your number :\n");
+    
+    scanf("%d", &x);
+    
+    if (x ==0) 
+        printf("0! = 1");
+    else 
+        printf("\n%d! = %d", x, factorial(x));
+        
+}
+
+int factorial(int x){
+    int numeral, f = 1;
+    
+    for (numeral=1 ;numeral <= x; ++numeral)
+        f = numeral * f;
+    return f;
+}
